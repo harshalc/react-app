a26ed02d102f8aa6c4729416aa938cb330f68609
harshalc89 <harshalc89@gmail.com>
2019-07-12 11:36:35 +0530

    facebook login

--- src/SidebarComponent.js
+++ src/SidebarComponent.js
@@ -1,12 +1,27 @@
 import React, { Component, Fragment, lazy, Suspense } from 'react';
 import LinkComponent from './LinkComponent';
 import TagsContainer from './TagsContainer';
+import FacebookLoginButton from './FacebookLoginButton';
 // import NameForm from './NameForm'
 const NameForm = lazy(() => import('./NameForm'));
 
 
 class SidebarComponent extends Component {
+    state = {
+        username: null
+    };
+
+    onFacebookLogin = (loginStatus, resultObject) => {
+        if (loginStatus === true) {
+            this.setState({
+                username: resultObject.user.name
+            });
+        } else {
+            alert('Facebook login error');
+        }
+    }
     render() {
+        const { username } = this.state;
         let linkContainer = [];
         if(this.props.navList){
             this.props.navList.forEach((value, index) => {
@@ -18,6 +33,19 @@ class SidebarComponent extends Component {
             <ul>
               {linkContainer}
             </ul>
+                <div className="App-intro">
+                    { !username &&
+                    <div>
+                        <p>Click on one of any button below to login</p>
+                        <FacebookLoginButton onLogin={this.onFacebookLogin}>
+                            <button>Facebook</button>
+                        </FacebookLoginButton>
+                    </div>
+                    }
+                    {username &&
+                    <p>Welcome back, {username}</p>
+                    }
+                </div>
             <div>
                 {/* <TagsContainer posts={this.props.posts}></TagsContainer> */}
                 <Suspense fallback={<div>Loading .....</div>}>
